66222632dffdc7d22c4f85341f318c98b8b77191
sillyaboy <liuhuating1234@gmail.com>
2023-09-04 22:37:56 +0800

    c

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,49 +10,63 @@ import SwiftUI
 struct ContentView: View {
     let emojis = ["😈","🤠","🎃","👻","🤠","🎃","👻","🤠","🎃","👻","🤠","🎃","👻"]
     @State var cardCount: Int = 4
+    
     var body: some View {
         VStack {
-            
-            HStack{
-                ForEach(0..<cardCount, id: \.self) { index in
-                    CardView(content: emojis[index])
-                }
-            }
-            .foregroundColor(.orange)
-            
-            
-            HStack{
-                Button(action:{
-                    if cardCount > 1 {
-                        cardCount -= 1
-                    }
-                    
-                }, label: {
-                    Image(systemName: "rectangle.stack.badge.minus.fill")
-                    Text("移除卡片")
-                })
-                .imageScale(.medium)
-                .font(.title2)
-                
-                Spacer()
-                
-                Button(action:{
-                    if cardCount < emojis.count {
-                        cardCount += 1
-                    }
-                    
-                }, label: {
-                    Image(systemName: "rectangle.stack.badge.plus.fill")
-                    Text("添加卡片")
-                })
-                .imageScale(.medium)
-                .font(.title2)
-            }
+            cards
+            cardCountAjusters
         }
         .padding()
-        
     }
+    
+    
+    var cardCountAjusters: some View {
+        HStack{
+            cardRemover
+            Spacer()
+            cardAdder
+        }
+    }
+    
+    var cards: some View {
+        HStack{
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+            }
+        }
+        .foregroundColor(.orange)
+    }
+    
+    var cardRemover: some View {
+        Button(action:{
+            if cardCount > 1 {
+                cardCount -= 1
+            }
+            
+        }, label: {
+            Image(systemName: "rectangle.stack.badge.minus.fill")
+            Text("移除卡片")
+        })
+        .imageScale(.medium)
+        .font(.title2)
+    }
+    
+    var cardAdder: some View {
+        Button(action:{
+            if cardCount < emojis.count {
+                cardCount += 1
+            }
+            
+        }, label: {
+            Image(systemName: "rectangle.stack.badge.plus.fill")
+            Text("添加卡片")
+        })
+        .imageScale(.medium)
+        .font(.title2)
+    }
+    
 }
+
 struct CardView: View {
     let content: String
     @State var isFaceUp = true
